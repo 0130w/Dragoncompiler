b89d501648bd0cd2301b1f3276b38eb2532c5684
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-03-18 02:42:44 +0000

    > make njusicpl24037 root Linux 1506af625000 5.15.49-linuxkit-pr #1 SMP PREEMPT Thu May 25 07:27:39 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  02:42:44 up  1:11,  0 users,  load average: 0.30, 0.06, 0.02 5624b33a8c5a8e68fc29aa0b5dd7d0a8b980590d

--- src/MyErrorListener.java
+++ src/MyErrorListener.java
@@ -10,18 +10,6 @@ import java.util.BitSet;
 public class MyErrorListener extends BaseErrorListener {
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-        throw new RuntimeException("line " + line + ":" + charPositionInLine + " " + msg);
-    }
-    @Override
-    public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
-        throw new RuntimeException("ambiguity");
-    }
-    @Override
-    public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
-        throw new RuntimeException("full context");
-    }
-    @Override
-    public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
-        throw new RuntimeException("context sensitivity");
+        System.err.println("Error Type A at line " + line + ": " + msg);
     }
 }
