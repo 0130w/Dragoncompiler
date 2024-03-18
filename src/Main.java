cf83b1836d5684efa66fe2839d25f05c376453b1
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-03-18 01:22:56 +0000

    > make njusicpl24037 root Linux 1506af625000 5.15.49-linuxkit-pr #1 SMP PREEMPT Thu May 25 07:27:39 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  01:22:56 up 0 min,  0 users,  load average: 0.05, 0.01, 0.00 d12d22131e247f377e00f89d469c8ceee668f64a

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main
 {    
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        String file_arg = args[0];
+        String[] file_parts = file_arg.split("=");
+        if (file_parts.length > 1) {
+            try {
+                String content = Files.readString(Path.of(file_parts[1]));
+                System.out.println(content);
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Usage: make run FILEPATH=...");
+        }
+    }
 }
