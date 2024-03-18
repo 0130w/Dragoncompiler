6425138f9e7c8f64ca95bc36917b9836f6c6d151
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-03-18 02:53:20 +0000

    > make njusicpl24037 root Linux 1506af625000 5.15.49-linuxkit-pr #1 SMP PREEMPT Thu May 25 07:27:39 UTC 2023 aarch64 aarch64 aarch64 GNU/Linux  02:53:20 up  1:22,  0 users,  load average: 0.03, 0.01, 0.00 b1e822b9b1bc6f52d98d4251d4489895c8c49a7

--- src/MyErrorListener.java
+++ src/MyErrorListener.java
@@ -11,7 +11,7 @@ public class MyErrorListener extends BaseErrorListener {
     boolean hasError = false;
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-        System.err.println("Error Type A at line " + line + ": " + msg + ".");
+        System.err.println("Error type A at Line " + line + ": " + msg + ".");
         hasError = true;
     }
 
